45d4eb8f69180cd1f4d817b9ae7dda28a4ab91db
Arjun Dhanjal <hello@arjundhanjal.com>
2015-03-16 19:23:15 -0400

    New-HomeDrive.ps1 creates a new user folder on the server’s user share and sets the correct folder permissions. It also adds the user to the correct Active Directory group to ensure that the user’s home drive will be mapped through GPO.

---
+++ New-HomeDrive.ps1
@@ -0,0 +1,42 @@
+PARAM(
+$Alias
+)
+
+$AccountName=$Alias
+
+#Assign the drive letter and home drive for a user in Active Directory
+
+    $HomeDrive='U:'
+    $UserRoot='\\NEBULA.ramp.dhanjalserver.arjundhanjal.com\USERDATA$\'
+    $HomeDirectory=$UserRoot+$AccountName
+
+#Add home drive information to Active Directory profile
+
+    # Set-ADUser $Alias -HomeDrive $HomeDrive -HomeDirectory $HomeDirectory
+
+#Create the folder on the root of the common USERDATA share
+
+    New-Item -Path $HomeDirectory -Type Directory -Force
+    $Domain='RAMP'
+    $IdentityReference=$Domain+'\'+$AccountName
+
+#Set paramaters for access rule
+
+    $FileSystemAccessRights=[System.Security.AccessCOntrol.FileSystemRights]"FullControl"
+    $InheritanceFlags=[System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
+    $PropagationFlags=[System.Security.AccessControl.PropagationFlags]"None"
+    $AccessControl=[System.Security.AccessControl.AccessControlType]"Allow"
+
+#Build access rule from parameters
+
+    $AccessRule=New-Object System.Security.AccessControl.FileSystemAccessRule -argumentlist ($IdentityReference,$FileSystemAccessRights,$InheritanceFlags,$PropagationFlags,$AccessControl)
+
+#Get current access rule from Home Folder for user
+
+    $HomeFolderACL=Get-ACL $HomeDirectory
+    $HomeFolderACL.AddAccessRule($AccessRule)
+    Set-ACL -Path $HomeDirectory -AclObject $HomeFolderACL
+
+#Add user to MAP-UserDrive group. This will invoke a GPO to map the user's home directory to U:\ upon login.
+
+    Add-ADGroupMember -Identity MAP-UserDrive -Member $AccountName
