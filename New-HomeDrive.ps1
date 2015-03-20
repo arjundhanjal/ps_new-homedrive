7a80be154e925093d6a85e61ec4ea4380185ac97
Arjun Dhanjal <hello@arjundhanjal.com>
2015-03-20 03:53:03 -0400

    Imported ActiveDirectory PS module and assigned AD group to a variable

--- New-HomeDrive.ps1
+++ New-HomeDrive.ps1
@@ -9,39 +9,44 @@ PARAM(
 
 $AccountName=$Alias
 
-#Assign the drive letter and home drive for a user in Active Directory
+# Importing ActiveDirectory PS module
+
+    Import-Module ActiveDirectory
+
+# Assign the drive letter and home drive for a user in Active Directory
 
     $HomeDrive='U:'
     $UserRoot='\\RAMPDC01.ramp.dhanjalserver.arjundhanjal.com\USERDATA$\'
     $HomeDirectory=$UserRoot+$AccountName
 
-#Add home drive information to Active Directory profile
+# Add home drive information to Active Directory profile
 
     # Set-ADUser $Alias -HomeDrive $HomeDrive -HomeDirectory $HomeDirectory
 
-#Create the folder on the root of the common USERDATA share
+# Create the folder on the root of the common USERDATA share
 
     New-Item -Path $HomeDirectory -Type Directory -Force
     $Domain='RAMP'
     $IdentityReference=$Domain+'\'+$AccountName
 
-#Set paramaters for access rule
+# Set paramaters for access rule
 
     $FileSystemAccessRights=[System.Security.AccessCOntrol.FileSystemRights]"FullControl"
     $InheritanceFlags=[System.Security.AccessControl.InheritanceFlags]"ContainerInherit, ObjectInherit"
     $PropagationFlags=[System.Security.AccessControl.PropagationFlags]"None"
     $AccessControl=[System.Security.AccessControl.AccessControlType]"Allow"
 
-#Build access rule from parameters
+# Build access rule from parameters
 
     $AccessRule=New-Object System.Security.AccessControl.FileSystemAccessRule -argumentlist ($IdentityReference,$FileSystemAccessRights,$InheritanceFlags,$PropagationFlags,$AccessControl)
 
-#Get current access rule from Home Folder for user
+# Get current access rule from Home Folder for user
 
     $HomeFolderACL=Get-ACL $HomeDirectory
     $HomeFolderACL.AddAccessRule($AccessRule)
     Set-ACL -Path $HomeDirectory -AclObject $HomeFolderACL
 
-#Add user to MAP-UserDrive group. This will invoke a GPO to map the user's home directory to U:\ upon login.
+# Add user to MAP-UserDrive group. This will invoke a GPO to map the user's home directory to U:\ upon login.
 
-    Add-ADGroupMember -Identity MAP-UserDrive -Member $AccountName
+    $ADGroup = "MAP-UserDrive"
+    Add-ADGroupMember -Identity $ADGroup -Member $AccountName
